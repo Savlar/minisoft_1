12d1a2a4261e99f72828ff5549a3dd505c62ce07
bl96 <baluch9@uniba.sk>
2020-10-15 23:43:13 +0200

    0.0.4

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ class Program:
         Main(self.canvas)
         win.mainloop()
 
-
 class Main:
     def __init__(self, canvas):
         self.canvas = canvas
@@ -22,9 +21,14 @@ class Main:
         self.transport_units_images = self.create_dictionary_for_images("textures/transportunits/", ["rocket", "ufo"])
         self.buttons_id = {}
 
+        #self.canvas.create_arc(50, 50, 400, 400, style=tkinter.ARC, width=10, start=180)
+
         self.create_buttons()
         self.buttons_bind = self.canvas.bind("<Motion>", self.filled_button)
         self.buttons_action_bind = self.canvas.tag_bind("button", "<Button-1>", self.buttonsAction)
+
+        self.create_rectangles()
+        Game(self.canvas, self.transport_units_images)
 
     def create_dictionary_for_images(self, path, image_list):
         images = {}
@@ -33,6 +37,10 @@ class Main:
             images[item] = tkinter.PhotoImage(file=f"{path}{item}.png")
 
         return images
+
+    def create_rectangles(self):
+        self.canvas.create_rectangle(10, 80, 1120, 580)
+        self.canvas.create_rectangle(1140, 80, 1350, 580)
 
     def create_buttons(self):
         x = 120
@@ -75,8 +83,54 @@ class Main:
 
 
 class Game:
-    def __init__(self, canvas):
+    def __init__(self, canvas, transport_units):
         self.canvas = canvas
+        self.transport_units = transport_units
+        self.transport_units_objects = []
+        self.results_transport_units = []
+        self.place_for_results_transport_units = self.canvas.create_rectangle(10, 600, 1120, 750, tag="units_place")
+        self.results_rectangle_coords = self.canvas.coords(self.place_for_results_transport_units)
+        self.movable_units = self.canvas.tag_bind("movable", "<B3-Motion>", self.move_transport_unit)
+        self.release_units = self.canvas.tag_bind("movable", "<ButtonRelease-3>", self.release_transport_unit)
+        self.click_units = self.canvas.tag_bind("movable", "<Button-1>", self.add_transport_unit_on_click)
+        self.create_transport_units()
+        self.canvas.update()
+
+    def add_transport_unit_on_click(self, event):
+        kind = "ufo"
+        if self.canvas.find_withtag("current")[0] == self.transport_units_objects[0]:
+            kind = "rocket"
+
+        self.results_transport_units.append(self.canvas.create_image(80 + len(self.results_transport_units) * 100, 680,image=(self.transport_units["rocket"] if kind == "rocket" else self.transport_units["ufo"]), tag="results_clickable"))
+
+    def clean_transport_units_objects(self):
+        for objc in self.transport_units_objects:
+            self.canvas.delete(objc)
+        self.transport_units_objects = []
+
+    def create_transport_units(self):
+        self.transport_units_objects.append(self.canvas.create_image(1240, 630, image=self.transport_units["rocket"], tag="movable"))
+        self.transport_units_objects.append( self.canvas.create_image(1250, 700, image=self.transport_units["ufo"], tag="movable"))
+
+    def remake_transport_units_objects(self):
+        self.clean_transport_units_objects()
+        self.create_transport_units()
+
+    def move_transport_unit(self, event):
+        self.canvas.coords("current", event.x, event.y)
+
+    def release_transport_unit(self, event):
+        current_coords = [event.x, event.y]
+        kind = "ufo"
+
+        print(self.transport_units_objects[0])
+        if self.canvas.find_withtag("current")[0] == self.transport_units_objects[0]:
+            kind = "rocket"
+
+        if self.results_rectangle_coords == current_coords or self.results_rectangle_coords[0] + 1110 >= current_coords[0] and self.results_rectangle_coords[0] - 1110 <= current_coords[
+            0] and self.results_rectangle_coords[1] + 150 >= current_coords[1] >= self.results_rectangle_coords[1] - 150:
+            self.results_transport_units.append(self.canvas.create_image(80+len(self.results_transport_units)*100, 680, image=(self.transport_units["rocket"] if kind == "rocket" else self.transport_units["ufo"]), tag="results_clickable"))
+        self.remake_transport_units_objects()
 
 
 class Settings:
